0f7eaa409de5109f57e59c0d77c5e63afeb07c0c
microtower00 <microtower00@gmail.com>
2021-12-09 10:36:11 +0100

    Aggiunto file explorer

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 #include <QLabel>
 #include<QVBoxLayout>
 #include<QPushButton>
+#include <QFileDialog>
 #include<QComboBox>
 
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent)
@@ -10,12 +11,16 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent)
     QLabel *oppure = new QLabel("oppure",this);
 
     QVBoxLayout *colonnaBott = new QVBoxLayout(this);
-    QPushButton *bottone1 = new QPushButton("Apri File");
+    QPushButton *bottone1 = new QPushButton("Apri file");
+    QFileDialog *file = new QFileDialog(this, "Open file", "C://");
     QPushButton *bottone2 = new QPushButton("Ottieni da OpenWeather");
     QComboBox *combo = new QComboBox();
 
+    connect(bottone1, SIGNAL(clicked()),file,SLOT(open()));
+
     colonnaBott->addWidget(titolo);
     colonnaBott->addWidget(bottone1);
+    //colonnaBott->addWidget(file);
     colonnaBott->addWidget(oppure);
     colonnaBott->addWidget(bottone2);
     colonnaBott->addWidget(combo);

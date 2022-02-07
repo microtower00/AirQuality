5a0b9411dfe0d630591f543d0dd1fa2ddab4b387
microtower00 <microtower00@gmail.com>
2022-02-07 09:55:00 +0100

    Terst new branch

--- mybarseries.h
+++ mybarseries.h
@@ -10,6 +10,6 @@ class MybarSeries : public QtCharts::QBarSeries
 {
 public:
     MybarSeries(const Dati&, QList<QString>, QObject *parent = nullptr);
-};
+};//Commit di prova prima di fottere tutto
 
 #endif // MYBARSERIES_H

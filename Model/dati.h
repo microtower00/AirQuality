bca2c0b9d75369dcc53fb3bf27d662f33a4e3267
microtower00 <microtower00@gmail.com>
2022-02-08 17:01:05 +0100

    Buttato giù firma del metodo per salvare i dati

--- Model/dati.h
+++ Model/dati.h
@@ -22,6 +22,7 @@ public:
     int columnCount(const QModelIndex &parent = QModelIndex()) const;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+    bool salvaJsonDati(const QString& path)const;
 private:
     QList<QMap<QString, double>> dati;
     QList<QString> chiavi;
